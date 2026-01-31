5a237ec45891585db2d511322e64ea7e155fbfa5
chaya <ch0533156857@gmail.com>
2026-01-31 20:46:46 +0200

    fix: dynamic api base-!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:3000/api'
-    apiUrl: 'https://wolftasksserver-pdaj.onrender.com/api'
-
+  apiUrl: 'http://localhost:3000/api'
 };
